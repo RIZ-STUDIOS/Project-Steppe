ac627769447d97e6984ebd77e633d701f167b117
Ricardo <rmmx03@gmail.com>
2024-01-25 22:59:09 +0000

    Player attack animation

---
+++ Assets/Scripts/PlayerAttackController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+namespace ProjectSteppe
+{
+    public class PlayerAttackController : MonoBehaviour
+    {
+        ThirdPersonController thirdPersonController;
+        private StarterAssetsInputs _input;
+        private int _animIDAttacking;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            thirdPersonController = GetComponent<ThirdPersonController>();
+        }
+
+        private void Start()
+        {
+            _input = GetComponent<StarterAssetsInputs>();
+
+            TryGetComponent(out _animator);
+            _animIDAttacking = Animator.StringToHash("Attacking");
+        }
+
+        private void Update()
+        {
+            Attack();
+        }
+
+        private void Attack()
+        {
+            if (!thirdPersonController.Grounded)
+            {
+                if(_input.attack) _input.attack = false;
+            }
+
+            if (_input.attack && thirdPersonController.canMove)
+            {
+                _animator.SetBool(_animIDAttacking, true);
+                thirdPersonController.canMove = false;
+            }else if (_input.attack)
+            {
+                _input.attack = false;
+            }
+        }
+    }
+}
