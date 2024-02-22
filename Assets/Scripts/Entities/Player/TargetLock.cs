3196c93600bcd3d6a7f39fea6f51dadafdbef433
Ricardo <rmmx03@gmail.com>
2024-02-22 21:37:03 +0000

    Fixed target lock being locked to object when it is destroyed

--- Assets/Scripts/Entities/Player/TargetLock.cs
+++ Assets/Scripts/Entities/Player/TargetLock.cs
@@ -79,6 +79,11 @@ namespace ProjectSteppe.Entities.Player
                 var d = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime).eulerAngles;
                 transform.rotation = Quaternion.Euler(0, d.y, 0);
             }
+
+            if(lockOn && !lookAtTransform)
+            {
+                StopLockOn();
+            }
         }
 
         private void StartLockOn()
