4c27c5d51606f8725f4799782e70f32819998c61
Ricardo <rmmx03@gmail.com>
2024-05-30 14:59:12 +0100

    Fixed animation issues

--- Assets/Scripts/Entities/Player/PlayerMovementController.cs
+++ Assets/Scripts/Entities/Player/PlayerMovementController.cs
@@ -271,23 +271,19 @@ namespace ProjectSteppe.Entities.Player
 
             if ((_input.move != Vector2.zero || dashing) && playerManager.HasCapability(PlayerCapability.Rotate))
             {
-                if (playerManager.PlayerTargetLock.lockOn)
+                if (dashing)
                 {
-                    if (dashing)
+                    targetRotation = playerCamera.transform.eulerAngles.y + Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+                }
+                else
+                {
+                    if (playerManager.PlayerTargetLock.lockOn)
                     {
-                        targetRotation = playerCamera.transform.eulerAngles.y + Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+                        targetRotation = playerCamera.transform.eulerAngles.y;
                     }
                     else
-                        targetRotation = playerCamera.transform.eulerAngles.y;
-                }
-                if (!dashing)
-                {
-                    if (!playerManager.PlayerTargetLock.lockOn)
                     {
                         targetRotation = playerCamera.transform.eulerAngles.y + Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
-                    }
-                    else
-                    {
                     }
                     this.moveDirection = inputDirection;
                 }
@@ -296,7 +292,7 @@ namespace ProjectSteppe.Entities.Player
                         RotationSmoothTime);
 
                 // rotate to face input direction relative to camera position
-                if (playerManager.PlayerTargetLock.lockOn && dashing)
+                if (dashing)
                     transform.rotation = Quaternion.Euler(0.0f, targetRotation, 0.0f);
                 else
                     transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
@@ -360,6 +356,7 @@ namespace ProjectSteppe.Entities.Player
         private void CameraRotation()
         {
             if (!virtualCamera.enabled) return;
+            if (playerManager.PlayerTargetLock.lockOn) return;
             // if there is an input and camera position is not fixed
             if (_input.look.sqrMagnitude >= _threshold)
             {
