1cbf236c73328a77b5805f2ab636dce39213fbd2
App24 <kado.fofinho2@gmail.com>
2024-06-03 18:38:17 +0100

    Balance bar in enemies

--- Assets/Scripts/Entities/EntityDeath.cs
+++ Assets/Scripts/Entities/EntityDeath.cs
@@ -1,5 +1,7 @@
+using ProjectSteppe.AI;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace ProjectSteppe.Entities
 {
@@ -9,9 +11,23 @@ namespace ProjectSteppe.Entities
 
         private Animator animator;
 
+        private EntityHealth health;
+
+        private Coroutine staggerCoroutine;
+
+        private NavMeshAgent navMeshAgent;
+
+        private AIController controller;
+
+        [SerializeField]
+        private float staggerLength;
+
         private void Awake()
         {
             animator = GetComponent<Animator>();
+            health = GetComponent<EntityHealth>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            controller = GetComponent<AIController>();
         }
 
         public void OnEntityDeath()
@@ -35,6 +51,33 @@ namespace ProjectSteppe.Entities
             deathFX.Play();
             while (deathFX.isPlaying) yield return null;
             Destroy(deathFX.gameObject);
+        }
+
+        public void StaggerTimeout()
+        {
+            if (staggerCoroutine != null)
+            {
+                StopCoroutine(staggerCoroutine);
+                staggerCoroutine = null;
+            }
+            staggerCoroutine = StartCoroutine(StaggerTimeoutIEnumerator());
+        }
+
+        public IEnumerator StaggerTimeoutIEnumerator()
+        {
+            health.vulnerable = true;
+            if (controller)
+                controller.enabled = false;
+            navMeshAgent.enabled = false;
+
+            yield return new WaitForSeconds(staggerLength);
+
+            health.vulnerable = false;
+
+            navMeshAgent.enabled = true;
+            if (controller)
+                controller.enabled = true;
+            animator.SetBool("PostureBreak", false);
         }
     }
 }
