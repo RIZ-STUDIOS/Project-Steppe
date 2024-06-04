b4e53a1679500e511eefa8a747f6cca2d2e6af2c
Ricardo <rmmx03@gmail.com>
2024-06-04 17:08:00 +0100

    Fix for weapon switching emission

---
+++ Assets/Scripts/AnimationState/TurnOffUnblockableStateMachineBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using ProjectSteppe.AI;
+using UnityEngine;
+
+namespace ProjectSteppe
+{
+    public class TurnOffUnblockableStateMachineBehaviour : StateMachineBehaviour
+    {
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var controller = animator.GetComponent<AIController>();
+            controller.TurnOffUnblockable();
+        }
+    }
+}
