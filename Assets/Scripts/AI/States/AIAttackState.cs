33f6fdb66e324da4fa5d7a7e4277d3cf0c5275f9
App24 <kado.fofinho2@gmail.com>
2024-06-04 13:49:51 +0100

    Enemy not attacking after stagger

--- Assets/Scripts/AI/States/AIAttackState.cs
+++ Assets/Scripts/AI/States/AIAttackState.cs
@@ -36,6 +36,7 @@ namespace ProjectSteppe.AI.States
 
         public virtual void OnForceExit()
         {
+            attackFinished = true;
             controller.AIEntity.EntityAttacking.CurrentWeapon.HideFernel();
             controller.animator.SetTrigger("ForceExit");
         }
