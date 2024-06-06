e57f07f54b4faef20cdbd992c77533561981684e
Zackaria <scehr@outlook.com>
2024-06-06 20:26:42 +0100

    Dispenser in.

---
+++ Assets/Scripts/Currencies/EntityCurrencyDispenser.cs
@@ -0,0 +1,37 @@
+using ProjectSteppe.Currencies;
+using ProjectSteppe.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace ProjectSteppe
+{
+    public class EntityCurrencyDispenser : CurrencyDispenser
+    {
+        [SerializeField]
+        private float dispenseDelay = 4;
+
+        private EntityHealth parentHealth;
+
+        private void Awake()
+        {
+            parentHealth = GetComponentInParent<EntityHealth>();
+            if (parentHealth != null) parentHealth.onKill.AddListener(OnParentDeath);
+        }
+
+        private void OnParentDeath()
+        {
+            StartCoroutine(DispenseCurrencyDelayed());
+        }
+
+        private IEnumerator DispenseCurrencyDelayed()
+        {
+            transform.SetParent(null);
+            yield return new WaitForSeconds(dispenseDelay);
+            parentHealth.mostRecentEntityHitBy.TryGetComponent(out CurrencyContainer container);
+            DispenseCurrencyPayloads(container);
+            Destroy(gameObject);
+        }
+    }
+}
